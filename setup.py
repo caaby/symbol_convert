4e344bc613a60841006f719ac8d075e8a150b68d
fangling <fangling@outlook.com>
2022-01-13 20:48:24 +0800

    commit

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name="symbol_convert",
-    version="0.1",
+    version="0.1.12",
     author="symbol_convert",
     author_email="fangling@outlook.com",
     description="替换掉字符串中的中文符号以及全角符号转换为半角符号",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Caaby/symbol_convert",
     packages=setuptools.find_packages(),
-    install_requires=['re'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
