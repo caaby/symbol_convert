5912dff4b624adba8c08ae6bfd6e5205eea36a64
A06840 <fangling@cyai.com>
2023-12-25 11:36:39 +0800

    add file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# extensions = []
+extensions = ['recommonmark', 'sphinx_markdown_tables']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -29,4 +29,4 @@ language = 'zh_CN'
 html_static_path = ['_static']
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-extensions = ['recommonmark', 'sphinx_markdown_tables']
+
